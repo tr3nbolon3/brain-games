efe60c9cb9786d958a604554cdbba219b5e0d2f3
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-06-02 14:15:38 +0600

    Added check for numbers less than 1 in isPrime function

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,6 +2,10 @@ import game from '../engine';
 import getRandomInt from '../utils';
 
 const isPrime = (num) => {
+  if (num <= 1) {
+    return false;
+  }
+
   for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) {
       return false;
